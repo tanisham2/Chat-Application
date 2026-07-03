6948ae8302609205a40fe9d9707b47623f192900
Tanisha Mathur <tanisha00002@gmail.com>
2026-07-03 14:07:38 +0530

    added usernames

--- app.js
+++ app.js
@@ -17,15 +17,30 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  //handle new messages
+  // Store username with socket
+  socket.username = 'Anonymous';
+  // Handle new messages with username
   socket.on('chat message', (msg) => {
-    console.log('Message received:', msg);
-      io.emit('chat message', msg);
+    io.emit('chat message', {
+      username: socket.username,
+      message: msg,
+      timestamp: new Date().toISOString()
     });
+  });
+  // Handle username change
+  socket.on('set username', (username) => {
+    const oldUsername = socket.username;
+    socket.username = username || 'Anonymous';
+    io.emit('user joined', {
+      oldUsername: oldUsername,
+      newUsername: socket.username
+    });
+  });
 
-  //handle disconnection
+  // Handle disconnection
   socket.on('disconnect', () => {
     console.log('A user disconnected');
+    io.emit('user left', { username: socket.username });
   });
 });
 
